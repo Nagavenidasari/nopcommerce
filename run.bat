34164dd01cc61aff92af624cb22b46604bfd5c6a
Nagavenidasari <109630216+Nagavenidasari@users.noreply.github.com>
2022-07-21 08:35:24 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,6 +4,7 @@ pytest -v --capture=tee-sys -m"sanity"  --html=Reports\sanityreportchrome.html T
 pytest -v --capture=tee-sys -m"sanity"  --html=Reports\sanityreportfirefox.html Testcases/ --browser firefox
 
 
-:: pytest -v --capture=tee-sys -m"regression"  --html=Reports\sanityreport.html Testcases/ --browser chrome
+
+pytest -v --capture=tee-sys -m"regression"  --html=Reports\regressionreport.html Testcases/ --browser chrome
 
 
